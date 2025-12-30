a421551d8dda33031afdd132afb4bacbde3e4266
sundar112 <kspsuraj44.ks@gmail.com>
2025-12-30 13:13:24 +0545

    2nd project

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<h1 className='bg-amber-950'>vite with tailedwind</h1>
+		</>
+	);
+}
+
+export default App;
